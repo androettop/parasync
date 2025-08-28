e309d8ab6a158af780d112dfbabb3946a7b65022
Pablo Androetto <androettop@gmail.com>
2025-08-28 21:05:18 +0200

    feat(audio): enhance SongAudioManager to manage audio duration and loading state

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -2,15 +2,24 @@ import { Loadable } from "excalibur";
 
 export class SongAudioManager implements Loadable<{}> {
   private _position = 0;
-  private _duration = -1;
+  private _duration;
   private _isPlaying = false;
+  private _isLoaded = false;
   private readonly songTrackPaths: string[];
   private readonly drumsTrackPaths: string[];
+
+  private _interval: number = 0;
+
   data: {} = {};
 
-  constructor(songTrackPaths: string[], drumsTrackPaths: string[]) {
+  constructor(
+    songTrackPaths: string[],
+    drumsTrackPaths: string[],
+    duration: number,
+  ) {
     this.songTrackPaths = songTrackPaths;
     this.drumsTrackPaths = drumsTrackPaths;
+    this._duration = duration;
   }
 
   get duration() {
@@ -30,22 +39,25 @@ export class SongAudioManager implements Loadable<{}> {
   }
 
   async load() {
+    this._isLoaded = false;
     const allPaths = [...this.songTrackPaths, ...this.drumsTrackPaths];
     await window.__TAURI_INTERNALS__.invoke("load_audio", {
       paths: allPaths,
     } as any);
     const st = await window.__TAURI_INTERNALS__.invoke("audio_status");
-    this._duration = st.duration_secs ?? -1;
     this._position = st.position_secs ?? 0;
+    this._isLoaded = true;
     return {};
   }
 
   isLoaded(): boolean {
-    return this._duration > 0;
+    return this._isLoaded;
   }
 
   async play() {
     await window.__TAURI_INTERNALS__.invoke("play_audio");
+    clearInterval(this._interval);
+    this._interval = window.setInterval(() => this.refreshStatus(), 32);
     this._isPlaying = true;
   }
 
@@ -69,8 +81,7 @@ export class SongAudioManager implements Loadable<{}> {
   // tip: refresca posición/duración periódicamente si lo necesitas
   async refreshStatus() {
     const st = await window.__TAURI_INTERNALS__.invoke("audio_status");
-    this._duration = st.duration_secs ?? this._duration;
+    console.log("status", st);
     this._position = st.position_secs ?? this._position;
-    this._isPlaying = !!st.is_playing;
   }
 }
